46279538e554445f360373072548c39ccc83349b
JamesN <jamesatnewton@gmail.com>
2023-03-08 16:12:22 +1000

    Change 2 a1 and a2, followed by a commit.

--- newupdate.py
+++ newupdate.py
@@ -4,8 +4,8 @@ import numpy as np
 import datetime as dt
 import click
 
-# This is change 1, a1
-# This is change 1, a2. These changes will be the first commit.
+# This is change 2, a1
+# This is change 2, a2. These changes will be the second commit.
 
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
